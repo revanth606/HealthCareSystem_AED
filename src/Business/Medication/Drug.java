73e484b4ea7f4ed18ef607c5f805458b60ffc4e1
Nihal Rao <nihalrao@Nihals-MacBook-Air.local>
2021-11-30 18:34:17 -0400

    added medication

---
+++ src/Business/Medication/Drug.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Medication;
+
+/**
+ *
+ * @author revanthkatha
+ */
+public class Drug {
+    private String drugName;
+    private int quantity;
+    private int cost;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getDrugName() {
+        return drugName;
+    }
+
+    public void setDrugName(String drugName) {
+        this.drugName = drugName;
+    }
+    
+}
